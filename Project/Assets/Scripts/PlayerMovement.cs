11bd2d70d7b8388129641822a7568a003fe484a5
m00dy123 <jvaerendonck@gmail.com>
2022-10-27 23:25:35 +0200

    .

--- Project/Assets/Scripts/PlayerMovement.cs
+++ Project/Assets/Scripts/PlayerMovement.cs
@@ -6,11 +6,13 @@ public class PlayerMovement : MonoBehaviour
 {
 
     public CharacterController2D controller;
+    public Animator animator;
 
     public float runSpeed = 40f;
 
     float horizontalMove = 0f;
     bool jump = false;
+ 
 
     // Start is called before the first frame update
     void Start()
@@ -23,10 +25,18 @@ public class PlayerMovement : MonoBehaviour
     {
        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
 
+       animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
        if(Input.GetButtonDown("Jump"))
        {
             jump = true;
+            animator.SetBool("IsJumping", true);
        }
+    }
+
+    public void OnLanding()
+    {
+        animator.SetBool("IsJumping", false);
     }
 
     void FixedUpdate ()
