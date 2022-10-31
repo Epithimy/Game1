133d0e67d31d0d84b401b1b27e50b0bb8a611fd1
Epithimy <jordicocbeast@gmail.com>
2022-11-01 00:17:47 +0100

    hearts

--- Project/Assets/Scripts/HealthSystem.cs
+++ Project/Assets/Scripts/HealthSystem.cs
@@ -1,18 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthSystem : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+   public int Health;
+   public int hearthAmount;
+   
+   public Image[] hearts;
+   public Sprite FullHeart;
+   public Sprite EmptyHeart;
 
-    // Update is called once per frame
-    void Update()
+   void Update() 
+   {
+
+    if (Health > hearthAmount)
     {
-        
+        Health = hearthAmount;
     }
+    
+    for (int i = 0; i < hearts.Length; i++)
+    {
+        if (i < Health) {
+            hearts[i].sprite = FullHeart;
+        } else
+        {
+            hearts[i].sprite = EmptyHeart;
+        }
+        
+        if (i < hearthAmount) 
+            {
+                hearts[i].enabled = true;
+            }
+            else
+            {
+                hearts[i].enabled = false;
+            }
+    }
+   }
 }
